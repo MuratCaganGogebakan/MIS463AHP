3be395996316ec3497748fa2d8d2da2c466eb4f2
cansupirsen <pirsencansu@gmail.com>
2022-12-12 21:16:38 +0300

    Ranges are correctly aligned.

--- frontend/script_dropdown.js
+++ frontend/script_dropdown.js
@@ -1,21 +1,25 @@
-const selectBtn = document.querySelector(".select-btn"),
-      items = document.querySelectorAll(".item");
+const selectBtns = document.querySelectorAll(".select-btn");
+const items = document.querySelectorAll(".item");
+selectBtns.forEach((btn) => {
 
-selectBtn.addEventListener("click", () => {
-    selectBtn.classList.toggle("open");
-});
+    btn.addEventListener("click", () => {
+        btn.classList.toggle("open");
+    });
+    items.forEach(item => {
+        item.addEventListener("click", () => {
+            let parentBtn = item.parentElement.parentElement;
+            item.classList.toggle("checked");
 
-items.forEach(item => {
-    item.addEventListener("click", () => {
-        item.classList.toggle("checked");
+            let title= parentBtn.id;
 
-        let checked = document.querySelectorAll(".checked"),
-            btnText = document.querySelector(".btn-text");
+            let checked = parentBtn.querySelectorAll(".checked"),
+                btnText = parentBtn.querySelector(".btn-text");
 
             if(checked && checked.length > 0){
                 btnText.innerText = `${checked.length} Selected`;
             }else{
-                btnText.innerText = "Select Genre";
+                btnText.innerText = title;
             }
-    });
-})
+        });
+    })
+});
