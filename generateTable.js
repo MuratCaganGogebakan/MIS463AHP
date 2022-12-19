ab09467aea3acea9ea7475d29ecb91eb8ba5adc4
cansupirsen <pirsencansu@gmail.com>
2022-12-20 00:26:52 +0300

    add index

--- generateTable.js
+++ generateTable.js
@@ -23,43 +23,47 @@ function generateTable(Data, headerArray) {
     // creates a table row
     const row = document.createElement("tr");
 
-    for (let j = 0; j < 9; j++) {
+    for (let j = 0; j < 10; j++) {
       const cellD = document.createElement("td");
       let cellH = document.createElement("th");
       let cellText = document.createTextNode(`cell in row ${i}, column ${j}`);
       if (j==0 && i!=0) {
+        cellText = document.createTextNode(i.toString());
+          
+      }
+      if (j==1 && i!=0) {
           cellText = document.createElement("img");
           cellText.setAttribute("src", Data[i - 1].image);
           // Set the scale of the image
           cellText.setAttribute("width", "175");
       }
-      if (j==1 && i!=0) {
+      if (j==2 && i!=0) {
           cellText.textContent = Data[i - 1].name.replace(/[^\x00-\x7F]/g, "");
       }
-      if (j==2 && i!=0) {
+      if (j==3 && i!=0) {
           cellText.textContent = Data[i - 1].price;
       }
-      if (j==3 && i!=0) {
+      if (j==4 && i!=0) {
           cellText.textContent = ((Data[i - 1].rating_percentage)/20).toFixed(1);
       }
-      if (j==4 && i!=0) {
+      if (j==5 && i!=0) {
           cellText.textContent = Data[i - 1].Genre;
       }
-      if (j==5 && i!=0) {
+      if (j==6 && i!=0) {
           cellText.textContent = Data[i - 1].publisher;
       }
-      if (j==6 && i!=0) {
+      if (j==7 && i!=0) {
           cellText.textContent = (parseFloat(Data[i - 1].playtime)/60).toFixed(1);
       }
-      if (j==7 && i!=0) {
+      if (j==8 && i!=0) {
           cellText.textContent = Data[i - 1].release_date;
       }
-      if (j==8 && i!=0) {
+      if (j==9 && i!=0) {
           cellText.textContent = Data[i - 1].platforms;
       }
       if (i == 0) {
           // set the text content to the header
-          cellH.textContent = headerArray[j];
+          cellH.textContent = headerArray[j-1];
           row.appendChild(cellH);
           row.classList.add("active-row");
       } else {
